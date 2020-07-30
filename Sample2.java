3c2e423709ad5586f4368f2dcef0c9006c1b0d3f
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-07-30 17:37:07 +0900

    変数の定義

---
+++ Sample2.java
@@ -0,0 +1,19 @@
+public class Sample2 {
+  public static void main(String[] args) {
+    String str = "Hello";
+    System.out.println(str);
+
+    int num = 3;
+    System.out.println(num);
+
+    double num1 = 5.0;
+    System.out.println(num1);
+
+    boolean b = true;
+    System.out.println(b);
+
+    String[] strs = {"Apple", "Banana"};
+    System.out.println(strs[0]);
+    System.out.println(strs[1]);
+  }
+}
