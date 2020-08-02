bd37f0538f509b53e32801fb5fdee9725fe5178c
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-02 22:34:00 +0900

    比較演算子

---
+++ Sample5.java
@@ -0,0 +1,30 @@
+public class Sample5 {
+  public static void main(String[] args) {
+    System.out.println(5 == 5);
+    System.out.println(5 == 7);
+    System.out.println("-----------");
+
+    System.out.println(5 != 5);
+    System.out.println(5 != 7);
+    System.out.println("-----------");
+
+    System.out.println(5 > 7);
+    System.out.println(7 > 5);
+    System.out.println("-----------");
+
+    System.out.println(5 < 7);
+    System.out.println(7 < 5);
+    System.out.println("-----------");
+
+    System.out.println(5 >= 7);
+    System.out.println(5 >= 5);
+    System.out.println(7 >= 5);
+    System.out.println("-----------");
+
+    System.out.println(5 <= 7);
+    System.out.println(5 <= 5);
+    System.out.println(7 <= 5);
+    System.out.println("-----------");
+
+  }
+}
