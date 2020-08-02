fa2a7bee224fd6fe30cda0391c4c73858bd9d564
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-02 22:48:21 +0900

    繰り返し処理

---
+++ Sample7.java
@@ -0,0 +1,15 @@
+public class Sample7 {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(i);
+    }
+
+    System.out.println("-----------");
+
+    int i = 0;
+    while (i <= 10) {
+      System.out.println(i);
+      i +=2;
+    }
+  }
+}
