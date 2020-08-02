182fb8924c5071c17bdf566ecc95eda18bccf69a
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-02 22:27:45 +0900

    論理演算子

---
+++ Sample4.java
@@ -0,0 +1,22 @@
+public class Sample4 {
+  public static void main(String[] args) {
+    System.out.println(true && true);
+    System.out.println(true && false);
+    System.out.println(false && false);
+    System.out.println("-----------");
+
+    System.out.println(true || true);
+    System.out.println(true || false);
+    System.out.println(false || false);
+    System.out.println("-----------");
+
+    System.out.println(true && true);
+    System.out.println(true && false);
+    System.out.println(false && false);
+    System.out.println("-----------");
+
+    System.out.println(!true);
+    System.out.println(!false);
+    System.out.println("-----------");
+  }
+}
