062f323bd1b9c819f65d24a6c6a066ced8a17d63
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-08 23:21:03 +0900

    Progate

---
+++ progate/Java1/Java1_8.java
@@ -0,0 +1,17 @@
+package progate.Java1;
+
+public class Java1_8 {
+  public static void main(String[] args) {
+    int number = 11;
+    String text = "Ruby";
+
+    System.out.println(number);
+    System.out.println(text);
+
+    number = 9;
+    System.out.println(number);
+
+    text = "Java";
+    System.out.println(text);
+  }
+}
---
+++ progate/Java1/Java1_9.java
@@ -0,0 +1,12 @@
+package progate.Java1;
+
+public class Java1_9 {
+  public static void main(String[] args) {
+    int number = 3;
+    System.out.println(number);
+
+    number += 7;
+
+    System.out.println(number);
+  }
+}
