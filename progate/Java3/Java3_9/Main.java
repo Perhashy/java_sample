2afb015c9bc9c1078c79a027ee920dae7f1a75d1
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-09 19:25:17 +0900

    Progate java3

---
+++ progate/Java3/Java3_9/Main.java
@@ -0,0 +1,7 @@
+package progate.Java3.Java3_9;
+
+public class Main {
+  public static void main(String[] args) {
+    Person.hello();
+  }
+}
---
+++ progate/Java3/Java3_9/Person.java
@@ -0,0 +1,7 @@
+package progate.Java3.Java3_9;
+
+public class Person {
+  public static void hello() {
+    System.out.println("Hello Java");
+  }
+}
