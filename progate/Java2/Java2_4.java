9b7270b0c99b5e84ed2de298d449f66d980608bc
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-09 15:54:12 +0900

    Progate java2

---
+++ progate/Java2/Java2_4.java
@@ -0,0 +1,22 @@
+package progate.Java2;
+
+public class Java2_4 {
+  public static void main(String[] args) {
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+
+    int num = 7;
+    if (num > 2) {
+      System.out.println("xは2より大きい");
+    }
+
+    if (num > 8) {
+      System.out.println("xは8より大きい");
+    }
+  }
+}
---
+++ progate/Java2/Java2_5.java
@@ -0,0 +1,15 @@
+package progate.Java2;
+
+public class Java2_5 {
+  public static void main(String[] args) {
+    int num = 13;
+
+    if (num < 10) {
+      System.out.println("10より小さい");
+    } else if (num < 20) {
+      System.out.println("10以上、20より小さい");
+    } else {
+      System.out.println("20以上");
+    }
+  }
+}
