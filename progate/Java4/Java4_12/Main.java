563f3d58e953f7454f2237e0e0634e7c1ccbb7d4
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-10 16:50:53 +0900

    Progate java4

---
+++ progate/Java4/Java4_12/Main.java
@@ -0,0 +1,9 @@
+package progate.Java4.Java4_12;
+
+public class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+
+    person1.printData();
+  }
+}
