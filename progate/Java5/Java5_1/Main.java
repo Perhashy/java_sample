8ae100bdcbae5bc3336224b32771f7a89b089528
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-10 18:01:08 +0900

    4.サブクラスをカスタマイズ

--- progate/Java5/Java5_1/Main.java
+++ progate/Java5/Java5_1/Main.java
@@ -1,7 +1,10 @@
 package progate.Java5.Java5_1;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     Car car = new Car();
     car.setName("フェラーリ");
     car.setColor("赤");
@@ -13,6 +16,11 @@ public class Main {
 
     System.out.println("【車の情報】");
     car.printData();
+    System.out.println("ガソリン量：" + car.getFuel() + "10L");
+    System.out.println("-----------------");
+    System.out.print("給油する量を入力してください：");
+    int litre = scanner.nextInt();
+    car.charge(litre);
 
     System.out.println("=================");
     System.out.println("【自転車の情報】");
