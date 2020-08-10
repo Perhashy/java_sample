23f4a3f78049de4eba5f54f55089338abe1f2ea6
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-10 17:53:28 +0900

    Progate java5

---
+++ progate/Java5/Java5_1/Car.java
@@ -0,0 +1,5 @@
+package progate.Java5.Java5_1;
+
+class Car extends Vehicle {
+  
+}
---
+++ progate/Java5/Java5_1/Main.java
@@ -0,0 +1,8 @@
+package progate.Java5.Java5_1;
+
+public class Main {
+  public static void main(String[] args) {
+    Car car = new Car();
+    Bicycle bicycle = new Bicycle();
+  }
+}
