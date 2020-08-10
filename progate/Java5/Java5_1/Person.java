161769cf8477e636b1e7f3a365d187f77ca417a9
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-10 20:17:02 +0900

    12.多態性

--- progate/Java5/Java5_1/Person.java
+++ progate/Java5/Java5_1/Person.java
@@ -41,10 +41,7 @@ public class Person {
     return this.weight / this.height / this.height;
   }
 
-  public void buy(Car car) {
-    car.setOwner(this);
-  }
-  public void buy(Bicycle bicycle) {
-    bicycle.setOwner(this);
+  public void buy(Vehicle vehicle) {
+    vehicle.setOwner(this);
   }
 }
