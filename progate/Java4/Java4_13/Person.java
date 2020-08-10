f8def1966b5d14856e8bb260ed45c8468b8176cb
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-10 17:00:03 +0900

    Progate java4

---
+++ progate/Java4/Java4_13/Person.java
@@ -0,0 +1,35 @@
+package progate.Java4.Java4_13;
+
+public class Person {
+  public static int count = 0;
+
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double height;
+  public double weight;
+
+  Person(String firstName, String lastName, int age, double height, double weight) {
+    count ++;
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  public String fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  public double bmiCalc() {
+    return this.weight / this.height / this.height;
+  }
+
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName() + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + this.bmiCalc() + "です");
+  }
+}
---
+++ progate/Java4/Java4_14/Main.java
@@ -0,0 +1,11 @@
+package progate.Java4.Java4_14;
+
+public class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+
+    person1.printData();
+
+    Person.printCount();
+  }
+}
