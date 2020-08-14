46afa6207aa7e3538f62a1fd2a376e79afb65984
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-14 20:39:50 +0900

    getNameメソッド

--- jankenSample/Main.java
+++ jankenSample/Main.java
@@ -2,6 +2,9 @@ package jankenSample;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println(Hand.Rock);
+    Player player1 = new Player("プレイヤー1");
+    Player player2 = new Player("プレイヤー2");
+    System.out.println(player1.getName());
+    System.out.println(player2.getName());
   }
 }
