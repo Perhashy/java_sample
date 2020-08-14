8b9c3d8cc9b0b689d1263fa0bad4c795940d3aa2
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-14 21:05:33 +0900

    Update Hand.java

--- jankenSample/Hand.java
+++ jankenSample/Hand.java
@@ -14,4 +14,13 @@ public enum Hand {
     }
     throw new IllegalStateException();
   }
+
+  public static Hand fromInt(int n) {
+    switch (n % 3) {
+      case 0: return Rock;
+      case 1: return Scissors;
+      case 2: return Paper;
+    }
+    throw new IllegalArgumentException(Integer.toString(n));
+  }
 }
