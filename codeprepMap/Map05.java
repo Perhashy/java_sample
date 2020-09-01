815ae126532f55e28e0bf755b89e0399c3b8dcbd
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-01 18:11:00 +0900

    Update Map05.java

--- codeprepMap/Map05.java
+++ codeprepMap/Map05.java
@@ -9,11 +9,14 @@ public class Map05 {
     map.put("a", "Apple");
     map.put("b", "Banana");
 
+    System.out.println("---------------------");
     System.out.println(map.getOrDefault("a", "Anime"));
     System.out.println(map.getOrDefault("c", "Cartoon"));
 
+    System.out.println("---------------------");
     System.out.println(map.size());
 
+    System.out.println("---------------------");
     System.out.println(map.containsKey("a"));
     System.out.println(map.containsKey("c"));
   }
