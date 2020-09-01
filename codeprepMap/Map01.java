cd00b252f4129460e383bf19fe367a20890764ba
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-01 17:54:12 +0900

    HashMapの作成

---
+++ codeprepMap/Map01.java
@@ -0,0 +1,11 @@
+package codeprepMap;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Map01 {
+  public static void main(String[] args) {
+    Map<String, String> map = new HashMap<>();
+    System.out.println(map);
+  }
+}
