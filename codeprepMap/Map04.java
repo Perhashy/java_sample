cca6961d8c6e7485658a34de8cd7007320fda0f3
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-01 17:59:37 +0900

    Mapとプリミティブ型

---
+++ codeprepMap/Map04.java
@@ -0,0 +1,11 @@
+package codeprepMap;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Map04 {
+  public static void main(String[] args) {
+    Map<Integer, String> map = new HashMap<>();
+    System.out.println(map);
+  }
+}
