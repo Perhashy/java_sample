cf9c05463d80d21b11f16f98a3813fee6defceac
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-01 17:57:58 +0900

    LinkedHashMapの作成

---
+++ codeprepMap/Map03.java
@@ -0,0 +1,11 @@
+package codeprepMap;
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class Map03 {
+  public static void main(String[] args) {
+    Map<String, String> map = new LinkedHashMap<>();
+    System.out.println(map);
+  }
+}
