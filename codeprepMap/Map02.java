5bf850947e2d93e172c6f202eab38a22bcf022dc
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-01 17:56:21 +0900

    TreeMapの作成

---
+++ codeprepMap/Map02.java
@@ -0,0 +1,11 @@
+package codeprepMap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Map02 {
+  public static void main(String[] args) {
+    Map<String, String> map = new TreeMap<>();
+    System.out.println(map);
+  }
+}
