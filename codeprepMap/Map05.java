3c37b72718eabd5c5f45206a2b0fa8488aca1f11
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-01 18:02:45 +0900

    put　Mapにキーと値をセット

---
+++ codeprepMap/Map05.java
@@ -0,0 +1,13 @@
+package codeprepMap;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Map05 {
+  public static void main(String[] args) {
+    Map<String, String> map = new HashMap<>();
+    map.put("a", "Apple");
+    map.put("b", "Banana");
+    System.out.println(map);
+  }
+}
