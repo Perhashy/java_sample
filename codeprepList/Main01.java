ba352ef390060dc3e60619944fb11601fef2fb38
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-02 21:40:52 +0900

    ArrayListの作成

---
+++ codeprepList/Main01.java
@@ -0,0 +1,11 @@
+package codeprepList;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main01 {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    System.out.println(list);
+  }
+}
