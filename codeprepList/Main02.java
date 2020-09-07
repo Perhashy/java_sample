054e6949b57d6e593fa046787147f4012ff0fafb
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-07 22:19:32 +0900

    要素の追加

---
+++ codeprepList/Main02.java
@@ -0,0 +1,13 @@
+package codeprepList;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main02 {
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    System.out.println(list);
+  }
+}
