76c11a893af72f080ff9cd8267376d2e24825959
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-07 22:14:35 +0900

    LinkedListの作成

--- codeprepList/Main01.java
+++ codeprepList/Main01.java
@@ -2,10 +2,14 @@ package codeprepList;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main01 {
   public static void main(String[] args) {
     List<String> list = new ArrayList<>();
     System.out.println(list);
+
+    List<String> list2 = new LinkedList<>();
+    System.out.println(list2);
   }
 }
