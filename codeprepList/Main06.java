f1f6368c14937d0a642145d3857250b45db3e7a3
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-14 18:40:55 +0900

    自然順序付けでのソート

---
+++ codeprepList/Main06.java
@@ -0,0 +1,19 @@
+package codeprepList;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main06 {
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>();
+    list.add(3);
+    list.add(1);
+    list.add(4);
+    list.add(2);
+    System.out.println(list);
+
+    System.out.println("---------------------");
+    list.sort(null);
+    System.out.println(list);
+  }
+}
