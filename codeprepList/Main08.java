fbe63f849af78ac045746ed6d69a0fb9c82df340
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-19 22:08:41 +0900

    キャパシティの指定

---
+++ codeprepList/Main08.java
@@ -0,0 +1,16 @@
+package codeprepList;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main08 {
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>(100);
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+
+    System.out.println(list);
+  }
+}
