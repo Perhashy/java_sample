4290eade2221501a26f12de8a9c00a50c37ff3ac
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-24 01:31:32 +0900

    2進数と16進数

--- codeprepUnicode/Main01.java
+++ codeprepUnicode/Main01.java
@@ -6,5 +6,16 @@ public class Main01 {
 
     System.out.println("---------------------");
     System.out.println(Math.pow(2, 16));
+
+    System.out.println("---------------------");
+    for (int i = 0; i <256; i++) {
+      System.out.println(
+        String.format("%3s", Integer.toString(i)) +
+        "：" +
+        String.format("%2s", Integer.toHexString(i)).replace(" ", "0").toUpperCase() +
+        "：" +
+        String.format("%8s", Integer.toBinaryString(i)).replace(" ", "0")
+      );
+    }
   }
 }
