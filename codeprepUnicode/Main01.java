c81cc4e4ff23f2eb4590702265f8ffe2264e3fef
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-24 01:25:16 +0900

    2バイト

--- codeprepUnicode/Main01.java
+++ codeprepUnicode/Main01.java
@@ -3,5 +3,8 @@ package codeprepUnicode;
 public class Main01 {
   public static void main(String[] args) {
     System.out.println(Math.pow(2, 8));
+
+    System.out.println("---------------------");
+    System.out.println(Math.pow(2, 16));
   }
 }
