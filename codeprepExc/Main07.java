c4a5274be0543684bb62537589912278433fd21b
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-27 20:21:53 +0900

    ArrayIndexOutOfBoundsException

--- codeprepExc/Main07.java
+++ codeprepExc/Main07.java
@@ -2,7 +2,11 @@ package codeprepExc;
 
 public class Main07 {
   public static void main(String[] args) {
-    String str = null;
-    System.out.println(str.toUpperCase());
+    // String str = null;
+    // System.out.println(str.toUpperCase());
+
+    System.out.println("---------------------");
+    int[] array = new int[] {5, 10, 15};
+    System.out.println(array[3]);
   }
 }
