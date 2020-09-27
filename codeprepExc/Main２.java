e8a8b80c129354c067a726660c4a9bc22b791346
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-27 18:47:47 +0900

    複数のExceptionのキャッチ

---
+++ codeprepExc/Main２.java
@@ -0,0 +1,29 @@
+package codeprepExc;
+
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Main２ {
+  private static String readFile(File file, String encoding) {
+    try {
+      return new String(Files.readAllBytes(file.toPath()), encoding);
+    } catch (NoSuchFileException e) {
+      System.out.println("ファイルが見つかりませんでした：" + file);
+      return "";
+    } catch (UnsupportedEncodingException e) {
+      System.out.println("エンコーディングが不正です：" + encoding);
+      return "";
+    } catch (IOException e) {
+      System.out.println("ファイルの読み込みに失敗しました：" + file);
+      return "";
+    }
+  }
+  public static void main(String[] args) {
+    System.out.println("テスト１：" + readFile(new File("file1.txt"), "utf-8"));
+    System.out.println("テスト２：" + readFile(new File("file2.txt"), "utf-8"));
+    System.out.println("テスト３：" + readFile(new File("file1.txt"), "unknown"));
+  }
+}
