3f665734415068fb7bafdfe169fb3815d0a43e18
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-09-27 20:09:37 +0900

    ファイルを読み込む関数(再)

---
+++ codeprepExc/Main05.java
@@ -0,0 +1,17 @@
+package codeprepExc;
+
+import java.nio.file.Files;
+import java.io.File;
+import java.io.IOException;
+
+public class Main05 {
+  private static String readFile(File file, String encoding) {
+    return new String(Files.readAllBytes(file.toPath()), encoding);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("テスト１：" + readFile(new File("file1.txt"), "utf-8"));
+    System.out.println("テスト２：" + readFile(new File("file2.txt"), "utf-8"));
+    System.out.println("テスト３：" + readFile(new File("file1.txt"), "unknown"));
+  }
+}
